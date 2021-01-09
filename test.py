0f311a7bf42853f7c85a7d3fa67ee363846af1e7
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-01-10 02:09:11 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from nets.ssd import SSD300
 
 if __name__ == "__main__":
     NUM_CLASSES = 21
-    input_shape = (512, 512, 3)
+    input_shape = (300, 300, 3)
     
     model = SSD300(input_shape, num_classes=NUM_CLASSES)
     model.summary()
